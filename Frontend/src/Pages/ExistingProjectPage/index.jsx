a9923a201b895b9e7e30ff3daec10759568687b0
codngGeeks <codinggeeksah@gmail.com>
2025-01-24 10:24:21 +0530

    Digital Resume

--- Frontend/src/Pages/ExistingProjectPage/index.jsx
+++ Frontend/src/Pages/ExistingProjectPage/index.jsx
@@ -210,7 +210,6 @@ function ExistingProj() {
                       }}
                       updateClick={handleAdd}
                     />{" "}
-                    \
                   </Grid>
                 </Grid>
               </Box>
